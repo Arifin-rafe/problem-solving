25312c75c5ef7de6e5c466dbf7e91c32b2c6d203
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-08 12:50:05 +0600

    solving challenge day 8

--- problem26.py
+++ problem26.py
@@ -7,5 +7,6 @@ a = set(int(x) for x in m if len(range(n)))
 b = set(int(x) for x in j if len(range(k)))
 
 sym_difference = a^b
-for x in sym_difference:
+sort_sym = sorted(sym_difference)
+for x in sort_sym:
     print(x)
