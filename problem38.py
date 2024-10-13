dd016037faa73b5ff99a3f90dabc87a43700938f
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-13 23:00:14 +0600

    solving challenge day 13 problem-38

--- problem38.py
+++ problem38.py
@@ -1,16 +1,15 @@
-n = int(input("Total num : "))
-t_f_list = []
-# num = []
-for _ in range(n):
-    m = input()
-    # print(type(m))
-    # num.append(float(m))
-    # for i in num:    
-    if type(m) == float:
-        t_f_list.append(True)
-    else:
-        t_f_list.append(False)
-print(t_f_list)
-# print(num)
-for i in t_f_list:
-    print(i)
+def is_float(value):
+    if value == "0":  
+        return False
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+n = int(input())  
+
+items = [input().strip() for _ in range(n)] 
+
+for item in items:
+    print(is_float(item))
