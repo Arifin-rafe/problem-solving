ba43966dececb4438a1ee2dc75ce251ece57deee
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-15 02:46:55 +0600

    solving challenge day 14 problem-41

--- problem41.py
+++ problem41.py
@@ -5,3 +5,5 @@ n = complex(input())
 c_match = cmath.polar(n)
 for i in c_match:
     print (i)
+    
+#In Python, the cmath.polar() function is used to convert a complex number into its polar coordinate representation. Polar coordinates represent a complex number in terms of its magnitude (or modulus, the distance from the origin) and phase angle (or argument, the angle made with the positive real axis).
