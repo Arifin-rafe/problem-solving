c9c97ead82c3e499b8e6d08b52077fbd45347a60
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-15 02:48:24 +0600

    solving challenge day 14 problem-41

--- problem41.py
+++ problem41.py
@@ -7,3 +7,5 @@ for i in c_match:
     print (i)
     
 #In Python, the cmath.polar() function is used to convert a complex number into its polar coordinate representation. Polar coordinates represent a complex number in terms of its magnitude (or modulus, the distance from the origin) and phase angle (or argument, the angle made with the positive real axis).
+
+#cmath.polar() is useful when working with complex numbers and when you need to switch from Cartesian (rectangular) form to polar form in mathematical or engineering applications.
