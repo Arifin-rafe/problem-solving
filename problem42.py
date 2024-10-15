43d3f2673c22e07dad338c141f0a075f3978d7c3
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-15 21:28:11 +0600

    solving challenge day 15 problem-42

---
+++ problem42.py
@@ -0,0 +1,11 @@
+from itertools import product
+import re
+a = input().split()
+b = input().split()
+a_list = [int(x) for x in a]
+b_list = [int(x) for x in b]
+itr = list(product(a_list,b_list))
+mt= ''
+for i in itr:
+    mt+=str(i) + " "
+print(mt)
