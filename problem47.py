22aba3dad13d8d1f41118e657873914dbbccb38c
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-19 01:45:57 +0600

    solving challenge day 18 problem-47

--- problem47.py
+++ problem47.py
@@ -1,6 +1,18 @@
-n = input().split()
-H = set("Hacker")
-R = set("Rank")
-H.update(R)
-print (H)
-set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+total_element = int(input())
+a = input().split()
+total_command = int(input())
+a_set = {int(x) for x in a}
+for _ in range(total_command):
+    command = input().split()
+    elements = input().split()
+    elements_set = {int(x) for x in elements}
+    if command[0] == "intersection_update":
+        a_set.intersection_update(elements_set)
+    elif command[0] == "update":
+        a_set.update(elements_set)
+    elif command[0] == "symmetric_difference_update":
+        a_set.symmetric_difference_update(elements_set)
+    elif command[0] == "difference_update":
+        a_set.difference_update(elements_set)
+dd = sum(a_set)
+print(dd)
