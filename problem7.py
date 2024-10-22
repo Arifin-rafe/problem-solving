a0ffe46ff9ef66822a6561c4c5085fd89bf14561
Arifin-rafe <arafinrafe90@gmail.com>
2024-10-23 01:02:41 +0600

    solving challenge day 23 problem-7 is solved now

--- problem7.py
+++ problem7.py
@@ -1,15 +1,6 @@
-# Given an integer, n, and n space-separated integers as input, create a tuple,t , of those  integers. Then compute and print the result of . 
 
-# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
-
-# Read the integer n
 n = int(input())
-
-# Read the space-separated integers and create a tuple
 numbers = map(int, input().split())
 t = tuple(numbers)
-
-# Compute and print the hash of the tuple
-
 print(hash(t))
 
